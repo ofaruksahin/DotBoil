c0e66170a56350b3af28d0f3498c15eb3a8a10ad
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2024-07-02 22:02:36 +0300

    Email package has been added EmailSettings

---
+++ src/DotBoil.Email/Configuration/EmailSettings.cs
@@ -0,0 +1,17 @@
+﻿using DotBoil.Configuration;
+using DotBoil.Email.Models;
+
+namespace DotBoil.Email.Configuration
+{
+    public class EmailSettings : IOptions
+    {
+        public string Key => "DotBoil:Email";
+
+        public IDictionary<string, ServerSettings> ServerSettings { get; set; }
+
+        public EmailSettings()
+        {
+            ServerSettings = new Dictionary<string, ServerSettings>();
+        }
+    }
+}
