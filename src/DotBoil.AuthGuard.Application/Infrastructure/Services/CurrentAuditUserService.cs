5c9c2ec0d376899acff6c4b546e854ffcb1267ff
ofaruksahin <ofaruksahin@outlook.com.tr>
2025-01-21 23:24:00 +0300

    Update CurrentAuditUserService.cs

--- src/DotBoil.AuthGuard.Application/Infrastructure/Services/CurrentAuditUserService.cs
+++ src/DotBoil.AuthGuard.Application/Infrastructure/Services/CurrentAuditUserService.cs
@@ -1,11 +1,21 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using DotBoil.EFCore;
+using Microsoft.AspNetCore.Http;
 
 namespace DotBoil.AuthGuard.Application.Infrastructure.Services;
 
 internal class CurrentAuditUserService : IAuditUser
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentAuditUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    
     public async Task<string> GetModifierName()
     {
-        return "";
+        return _httpContextAccessor.HttpContext.User.Claims?.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.PreferredUsername)?.Value;
     }
 }
