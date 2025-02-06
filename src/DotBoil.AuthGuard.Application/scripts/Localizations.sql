81a531a511d6bc5dc7d3995460f1f974819dabab
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2025-02-06 22:52:52 +0300

    AuthGuard projesi için gerekli olan scriptler eklendi

---
+++ src/DotBoil.AuthGuard.Application/scripts/Localizations.sql
@@ -0,0 +1,162 @@
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(1, 'EN', 'Login', 'PageTitle', 'AuthGuard Sign In');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(2, 'TR', 'Login', 'PageTitle', 'AuthGuard Giriş Yap');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(3, 'EN', 'Login', 'SignInTitle', 'Sign in to account');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(4, 'TR', 'Login', 'SignInTitle', 'Hesabına giriş yap');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(5, 'EN', 'Login', 'SignInSubTitle', 'Enter your email & password to login');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(6, 'TR', 'Login', 'SignInSubTitle', 'Giriş yapmak için email ve şifrenizi giriniz');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(7, 'EN', 'Login', 'EmailAddress', 'Email Address');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(8, 'TR', 'Login', 'EmailAddress', 'Email Adresi');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(9, 'EN', 'Login', 'Password', 'Password');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(10, 'TR', 'Login', 'Password', 'Şifre');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(11, 'EN', 'Login', 'ForgotPassword', 'Forgot password?');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(12, 'TR', 'Login', 'ForgotPassword', 'Şifrenizi mi unuttunuz?');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(13, 'EN', 'Login', 'SignIn', 'Sign in');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(14, 'TR', 'Login', 'SignIn', 'Giriş yap');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(15, 'EN', 'Login', 'DontHaveAccount', 'Don''t have account?');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(16, 'TR', 'Login', 'DontHaveAccount', 'Hesabınız yok mu?');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(17, 'EN', 'Login', 'CreateAccount', 'Create Account');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(18, 'TR', 'Login', 'CreateAccount', 'Hesap Oluştur');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(19, 'TR', 'Login', 'OrSignInWith', 'Veya Giriş Yapın');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(20, 'EN', 'Login', 'OrSignInWith', 'Or Sign in with');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(21, 'TR', 'Login', 'InvalidEmailOrPassword', 'Email veya şifre hatalı');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(22, 'EN', 'Login', 'InvalidEmailOrPassword', 'Invalid email or password');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(23, 'TR', 'Login', 'AuthorizationFailed', 'Oturum Açılamadı');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(24, 'EN', 'Login', 'AuthorizationFailed', 'Authorization Failed');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(25, 'EN', 'Login', 'UserAlreadyExists', 'The user is already registered, please log in');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(26, 'TR', 'Login', 'UserAlreadyExists', 'Kullanıcı zaten kayıtlı, lütfen giriş yapınız');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(27, 'TR', '', 'Success', 'İşleminiz başarıyla tamamlandı');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(28, 'EN', '', 'Success', 'Your operation has been successfully completed');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(29, 'TR', 'Login', 'UserNotFound', 'Kullanıcı bulunamadı');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(30, 'EN', 'Login', 'UserNotFound', 'User not found');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(31, 'EN', '', 'OtpCodeInvalid', 'The OTP code is incorrect');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(32, 'TR', '', 'OtpCodeInvalid', 'OTP kodu hatalı');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(33, 'TR', '', 'OtpCodeExpired', 'OTP kodunun süresi doldu');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(34, 'EN', '', 'OtpCodeExpired', 'The OTP code has expired.');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(35, 'TR', 'Login', 'RegisterTitle', 'Yeni hesap oluştur');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(36, 'EN', 'Login', 'RegisterTitle', 'Create a new account');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(37, 'TR', 'Login', 'Name', 'Ad');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(38, 'EN', 'Login', 'Name', 'Name');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(39, 'TR', 'Login', 'Surname', 'Soyad');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(40, 'EN', 'Login', 'Surname', 'Surname');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(41, 'TR', 'AuthGuard', 'Title', 'AuthGuard Oturum Yönetim Sistemi');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(42, 'EN', 'AuthGuard', 'Title', 'AuthGuard Authorization Management System');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(43, 'TR', 'Login', 'ForgotPasswordSubTitle', 'Email ile adresi ile şifrenizi yenileyin');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(44, 'EN', 'Login', 'ForgotPasswordSubTitle', 'Renew your password with your email address');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(45, 'TR', 'Login', 'RenewPassword', 'Şifremi yenile');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(46, 'EN', 'Login', 'RenewPassword', 'Reset my password');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(47, 'TR', 'Login', 'ForgotPasswordTitle', 'Şifremi Unuttum');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(48, 'EN', 'Login', 'ForgotPasswordTitle', 'Forgot Password');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(49, 'TR', 'Login', 'VerifyOtpTitle', 'Otp kodunu doğrula');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(50, 'EN', 'Login', 'VerifyOtpTitle', 'Verify otp code');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(51, 'TR', 'Login', 'OtpCode', 'Doğrulama Kodu');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(52, 'EN', 'Login', 'OtpCode', 'Otp Code');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(53, 'TR', 'Login', 'ResendOtp', 'Yeniden otp kodu gönder');
+INSERT INTO DotBoilAuthGuard.Localizations
+(Id, `Language`, `Group`, `Key`, Value)
+VALUES(54, 'EN', 'Login', 'ResendOtp', 'Resend otp code');
---
+++ src/DotBoil.AuthGuard.Application/scripts/Parameters.sql
@@ -0,0 +1,6 @@
+INSERT INTO DotBoilAuthGuard.Parameters
+(Id, `Section`, `Key`, Value, TenantId)
+VALUES(1, 'AuthGuard', 'Logo', '/assets/images/logo/logo.png', 0);
+INSERT INTO DotBoilAuthGuard.Parameters
+(Id, `Section`, `Key`, Value, TenantId)
+VALUES(2, 'AuthGuard', 'LogoDark', '/assets/images/logo/logo_dark.png', 0);
